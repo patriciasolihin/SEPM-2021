b76f30bc8c51f51344a239369726a32cffcb6076
Tim760 <s3658451@student.rmit.edu.au>
2021-09-27 13:53:16 +1000

    server is operational, can use static files and do routing

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-'use strict';
+const express = require('express');
+const app = express();
 
-// use http and fileSystem modules
-let http = require('http');
-let fs = require('fs');
+// server can now use static files (css, images etc.)
+app.use(express.static('public'))
 
-// Server responses/callbacks 
-function requestCallback (request, response) {
-    fs.readFile('mapMode.html', function (error, data) {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(data);
-        return response.end();
-    })
-};
+// load home page file with directory http:localhost(8000)/
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/homePage.html');
+});
 
-// create the server and run the callback
-let server = http.createServer(requestCallback);
-server.listen(8000);
+// load map mode page file with directory http:localhost(8000)/mapMode.html
+app.get('/mapMode.html', function (request, response) {
+    response.sendFile(__dirname + '/mapMode.html');
+});
 
-// show the system is running for devs
-console.log("Server is listening...");
+app.listen(8000);
 
-
+console.log("Server Listening ...");
