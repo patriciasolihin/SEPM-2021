6f398badaadd1c5a042afe7903d84b43af0e3188
Tim760 <s3658451@student.rmit.edu.au>
2021-10-04 05:43:40 +1100

    Integrated Key logos to Map, added server error page handling, uniformed the styling and fixed the footer spacing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SEPM-2021
 Source code for SEPM B3-15
+Instructions:
+1.) Open your command prompt or linux bash interperator (Ubuntu for Windows, Mac terminal is Linux)
+2.) Find the directory with the file "server.js" in your console e.g. ~/projects/SEPM/SEPM-2021$
+3.) Enter: node server.js
+4.) Your console should now say: Server Listening ... 
+5.) Open a Browser and go to: http:localhost(8000)/
--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.get('/mapMode.html', function (request, response) {
     response.sendFile(__dirname + '/mapMode.html');
 });
 
+// Page not Found Error (Error 404)
+app.use(function(request, response, next) {
+    response.status(404).sendFile(__dirname + '/page404.html');
+});
+
+// Internal Server Error (Error 500)
+app.use((error, request, response, next) => {
+    response.status(500).sendFile(__dirname + '/page500.html');
+});
+
 app.listen(8000);
 
 console.log("Server Listening ...");
