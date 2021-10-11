7c1a0ea0323f1a0b83b6dc853664dd74352a0bd9
Nanako <62985747+s3845837@users.noreply.github.com>
2021-10-11 16:44:05 +1100

    Patricia's work

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+	include("header.inc");
+?>
+<?php
+	include("nav.inc");
+?>
+	
+	<main>
+	<table border=0 id="bookbutton">
+	<tr><td><a href="https://portal.cvms.vic.gov.au/book/"><img src="1.png"></a></td><td><a href="https://www.coronavirus.vic.gov.au/where-get-tested-covid-19"><img src="2.png"></a></td></tr>
+	<tr><td colspan=2><a href="https://portal.cvms.vic.gov.au/#"><img src="3.png"></a></td></tr>
+	</table>
+	</main>
+	
+<?php include("footer.inc"); 
+?>
